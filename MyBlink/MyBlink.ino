f7ac4ed6b8a685b642ef0775d519cc46e4319f57
aki-ttb <54969321+aki-ttb@users.noreply.github.com>
2023-12-10 06:25:56 +0900

    Add files via upload
    LEDのON/OFF時にシリアルモニタでメッセージ表示を追加

--- MyBlink/MyBlink.ino
+++ MyBlink/MyBlink.ino
@@ -26,12 +26,15 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);   //initialize serial monitor
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  Serial.println("LED ON");         // serial monitor display "LED ON"
   delay(5000);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  Serial.println("LED OFF");         // serial monitor display "LED OFF"
   delay(1000);                      // wait for a second
 }
